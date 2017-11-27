c6537a3027342bbed2263350fd2e9f2c39c9b336
alexis papagiannopoulos <alexis papagiannopoulos>
2017-11-27 15:07:49 +0100

    causing conflict

--- implicit_fairing/mesh_processing.cpp
+++ implicit_fairing/mesh_processing.cpp
@@ -119,7 +119,7 @@ std::pair<size_t, size_t> get_intervals_borders(float a, float b, float l, float
 	// Use std::pair to return the indices of the first and the last interval border.
 	
 
-	//Adding another comment line add asda
+	//conflict line 
 
 	return intervals_borders;
 }
